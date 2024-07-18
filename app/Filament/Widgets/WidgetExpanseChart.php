b5f51b1882bc4dd9468ad6acf42011326cf07b9d
eri-maulana <maulanabouble11@gmail.com>
2024-07-18 18:00:05 +0700

    06 Membuat chart pada dashboard

---
+++ app/Filament/Widgets/WidgetExpanseChart.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Transaction;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Filament\Widgets\ChartWidget;
+
+class WidgetExpanseChart extends ChartWidget
+{
+    protected static ?string $heading = 'Pengeluaran';
+    protected static string $color = 'danger';
+
+    protected function getData(): array
+    {
+        $data = Trend::query(Transaction::expanses())
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perDay()
+            ->sum('amount');
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Pengeluaran per Hari',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
